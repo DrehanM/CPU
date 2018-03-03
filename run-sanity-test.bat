0db37d2ba2ab48c2487f01b665784b14e66a11e0
Stephan K <thaumicmekanism@gmail.com>
2018-03-02 16:58:03 -0800

    Updated the batch file to be more efficient.
    Since the python script is now python3 compatible. I have the batch script check for either python 2 or 3 in multiple common directories.

--- run-sanity-test.bat
+++ run-sanity-test.bat
@@ -1,27 +1,36 @@
 @echo off
+setlocal enabledelayedexpansion
 :: Just run this file and you can test your circ files!
 :: Make sure your files are in the directory above this one though!
 :: Credits to William Huang and Stephan Kaminsky
 
-::!!!!!!!!!!!!SET the python 2.7 location here. Just repace "nul" with with the directory to python ex: "C:\Python27\python.exe"!!!!!!
-set python27path="null"
+::!!!!!!!!!!!!SET the python location here. Just repace "nul" with with the directory to python ex: "C:\Python27\python.exe"!!!!!!
+set pythonpath="null"
 
 
 ::Leave this alone
-set defname=python2.7
-set defpy="C:\Python27\python.exe"
+set defname=python
+set defname2=python2.7
+set defname3=python3
+set defpy=C:\Python\
+set defpy2=C:\Python27\
+set defpy3=C:\Python3\
+set defn=%defname% %defname3% %defname2%
+set defn_size=3
+set defp=%defpy% %defpy3% %defpy2%
+set defp_size=3
 
 copy alu.circ tests>nul
 IF %ERRORLEVEL% NEQ 0 (
     echo.
-    ECHO [ERROR] Copy failed: alu.circ to tests!
+    ECHO [ERROR] Copy failed: alu.circ to tests^^!
     if exist tests/alu.circ echo Found an alu.circ file in the test folder already. &goto choicea
     goto end
     :choicea
     set /P c=Do you want to use it [Y/N]?
     if /I "%c%" EQU "Y" goto :cpreg
     if /I "%c%" EQU "N" goto :end
-    echo Please only enter y or n!
+    echo Please only enter y or n^^!
     echo.
     goto :choicea
 )
@@ -29,54 +38,73 @@ IF %ERRORLEVEL% NEQ 0 (
 copy regfile.circ tests>nul
 IF %ERRORLEVEL% NEQ 0 (
     echo.
-  ECHO [ERROR] Copy failed: regfile.circ to tests!
+  ECHO [ERROR] Copy failed: regfile.circ to tests^^!
   if exist tests/regfile.circ echo Found an regfile.circ file in the test folder already. &goto choiceb
     goto end
     :choiceb
     set /P c=Do you want to use it [Y/N]?
     if /I "%c%" EQU "Y" goto :next
     if /I "%c%" EQU "N" goto :end
-    echo Please only enter y or n!
+    echo Please only enter y or n^^!
     echo.
     goto :choiceb
 )
 :next
 cd tests
-::Determining python location
-where %defname%>nul
-IF %ERRORLEVEL% NEQ 0 goto DefaultLocation
-:inPath
-echo Python 2.7 found in path!
-%defname% sanity_test.py
-goto end
-:DefaultLocation
-ECHO [Warning] Python 2.7 is not in the path! Checking alternative locations...
-echo.
-if not exist %defpy% goto custloc
-echo.
-echo [Default Location] Python found!
-echo.
-%defpy% sanity_test.py
-goto end
+
+
+::Check if python is installed and located in the path with common names
+set pyloc="null"
+:pathpython
+(for %%a in (%defn%) do ( 
+    echo [Info] Checking for %%a...
+    WHERE /q %%a && set pyloc=%%a&&goto runscript
+    echo [Warning] %%a not found in path.
+    echo.
+))
+echo [Warning] Python was not found in the path^^! Checking alternative locations...
+goto defaultpython
+
+::If we got here, we now need to check if python is in common locations on the computer
+
+:defaultpython
+(for %%a in (%defp%) do (
+    (for %%b in (%defn%) do (
+        echo [Info] Checking for "%%a%%b.exe"
+        if exist "%%a%%b.exe" set pyloc="%%a%%b.exe"&goto runscript
+        echo [Warning] "%%a%%b.exe" not found.
+        echo.
+    ))
+))
+echo [Warning] Python was not found in the default install location^^! Checking for custom set location...
+goto custloc
+
+::If python is not in a default install location, we will check to see if 
 :custloc
-echo [Warning] Python 2.7 was not found in %defpy%. Checking custom location...
+set pyloc=%pythonpath%
+if not exist %pyloc% goto nend
+goto runscript
+
+
+::This is called if a python executable was found!
+:runscript
+if %pyloc%=="null" goto noPython
+cls
+echo %pyloc% found^^! Running python script...
 echo.
-if %python27path%=="null" goto noPython
-if not exist %python27path% goto nend
-echo.
-echo [Custom Location] Python found!
-echo.
-%python27path% sanity_test.py
+%pyloc% sanity_test.py
 goto end
+
+
 :noPython
 echo. 
-echo [Error] Please open up the batch script and set the %python27path% with the path to the python 2.7 executable.
+echo [Error] Please open up the batch script and set the 'pythonpath' with the path to the python executable.
 echo It could not be found on the system so you need to set it yourself before this will work. 
 echo. 
 goto end
 :nend
 echo.
-echo [Error] Python 2.7 was not found in %python27path%!.
+echo [Error] Python was not found in %pyloc%^^!.
 :end
 cd ..
 echo.
